9e06b202a24da46355319c7afae35f0885116cb5
Kavleen <kavleen.sabharwal@venwiz.com>
2024-03-06 12:47:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/articles" element={<Articles />} />
-				<Route path="/article/:slug" element={<ReadArticle />} />
+				{/* <Route path="/projects" element={<Projects />} /> */}
+				{/* <Route path="/articles" element={<Articles />} /> */}
+				{/* <Route path="/article/:slug" element={<ReadArticle />} /> */}
 				<Route path="/contact" element={<Contact />} />
 				<Route path="*" element={<Notfound />} />
 			</Routes>

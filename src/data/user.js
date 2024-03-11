c7e150daef011ccbe7f0a66bb8b0e6f3a9f2215f
Kavleen <kavleen.sabharwal@venwiz.com>
2024-03-11 19:11:30 +0530

    intro change

--- src/data/user.js
+++ src/data/user.js
@@ -12,9 +12,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web developer",
+		title: "Full-stack developer",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a developer with expertise in Javascript. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {

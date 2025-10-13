dedff452c4b4f2af458317fc33b0abc27d57dd2f
Kavleen Sabharwal <kashishsabharwal@gmail.com>
2025-10-13 15:44:53 +0530

    .

--- src/components/common/mobileNav.jsx
+++ src/components/common/mobileNav.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-  faUser,
   faFolderOpen,
   faCogs,
   faFileAlt,
